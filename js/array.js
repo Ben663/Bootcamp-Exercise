8760bd6bab1376107e155224df58660600aed171
Ben663 <87435130+Ben663@users.noreply.github.com>
2022-09-21 00:51:32 +0300

    .

---
+++ js/array.js
@@ -0,0 +1,8 @@
+const people = ["Greg" , "Mary", "Devon", "James"];
+//1 console.log(people)
+//2
+//people.shift("Greg");
+//3 
+
+
+console.log(people);
--- js/operators.js
+++ js/operators.js
@@ -3,12 +3,11 @@ const prompt = require("prompt-sync")();
 function countryToLiveIn(language, isIsland, population, country) {
     if (language =='english' && population < 50 && !isIsland)
     {
-        console.log('You should live in ',country);
+        console.log(`You should live in ${country}`);
     }
     else
     {
-        // console.log('does not meet your criteria',country);
         console.log(`${country} does not meet your criterai`);
     }
 }
-countryToLiveIn('english',false,49,'spain');
+countryToLiveIn('english',false,54,'spain');
