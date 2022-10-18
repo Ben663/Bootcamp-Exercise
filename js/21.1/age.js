2290e4aa641c4e1eea52db9c710b6cbecb272e0e
Ben663 <87435130+Ben663@users.noreply.github.com>
2022-10-18 23:03:00 +0300

    221018

---
+++ js/21.1/age.js
@@ -0,0 +1,15 @@
+/*
+Create a webpage that has an input field for the user’s age and
+a button. If the user enters a number above 18, make the text
+“you can drink appear”. If the user enters a number below 18,
+make the text “you’re too young” appear.
+*/
+const ageup = document.querySelectorAll('.number')
+function age (number)  {
+    return (input.number == '18') ? 'you can drink appear' : 'you to young '
+    
+
+}
+
+
+
---
+++ js/22.2/form.js
@@ -0,0 +1,6 @@
+const form = document.querySelector('#sing-up');
+
+form.addEventListener('submit', function(e) {
+    alert('congratulations you successfully sent this form');
+   // e.preventDefault();
+})
