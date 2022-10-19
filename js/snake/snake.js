e649e24599a1168073a8318e962e968318e3cb75
Ben663 <87435130+Ben663@users.noreply.github.com>
2022-10-19 16:14:14 +0300

    221019

--- js/snake/snake.js
+++ js/snake/snake.js
@@ -1,19 +1,15 @@
-import { getInputDirection } from "./input"
+import { getInputDirection } from "./input.js"
 
-export const snakeSpeed = 1
-const snakeBody =  [
-    { x: 10,  y:11},
-    { x: 11,  y:11},
-    { x: 12,  y:11}
-]
+export const snakeSpeed =5
+const snakeBody =  [{ x: 11,  y: 11}]
 
 export function update () {
-    getInputDirection()
+    const inputDirection = getInputDirection()
     for (let i = snakeBody.length - 2; i >= 0; i--) {
         snakeBody[i + 1] = {...snakeBody[i]}
     }
-    // snakeBody[0].x += 0
-    // snakeBody[0].y += 1
+    snakeBody[0].x +=inputDirection.x
+    snakeBody[0].y += inputDirection.y
 }
 
 export function draw (gameBoard) {
