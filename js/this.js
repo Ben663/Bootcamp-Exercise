4254627bdc5857b46f39b64fbc6db5c24a516eef
Ben663 <87435130+Ben663@users.noreply.github.com>
2022-10-23 16:56:17 +0300

    221023

---
+++ js/this.js
@@ -0,0 +1,65 @@
+//Answer the following questions:
+
+/*
+Question 1:
+In your own words what will this point to and why?
+(Note this is the global scope)
+console.log(this);
+
+return the method this in the global
+*/
+
+
+/*
+Question 2:
+a. In your own words what will this point to and why?
+b. How can you fix this code?
+
+const myObj = {
+name: "Timmy",
+greet () {
+    console.log(`Hello ${this.name}`);
+}
+};
+myObj.greet();
+//return the string `hello`
+//cancel the : end the =>
+*/
+
+/*
+Question 3:
+In your own words what will this point to and why?
+
+const myFuncDec = function () {
+console.log(this);
+};
+//return the stuff in the function
+*/
+
+
+/*
+Question 4:
+In your own words what will this point to and why?
+
+const myFuncArrow = () => {
+console.log(this);
+};
+myFuncArrow();
+//return the stuff in the arrow function
+*/
+
+/*
+Question 5:
+a. In your own words, what will this point to and why?
+b. How can you fix this code?
+
+document.querySelector(".element").addEventListener('click', function(){
+console.log(this);
+});
+//return the stuff in the arrow function
+//add a click and a function
+*/
+
+
+
+
