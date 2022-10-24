55f2f9e73bf8b5e934fe75c76ec151aa889994e9
Ben663 <87435130+Ben663@users.noreply.github.com>
2022-10-24 22:37:06 +0300

    221024

---
+++ js/isSquare.js
@@ -0,0 +1,18 @@
+function Square(a, b, c, d) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.d = d;
+  }
+  
+  Square.prototype.isSquare = function (a, b, c, d) {
+    if (a === b && b === c && c === d) {
+      return true;
+    } else return false;
+  };
+  
+  const newSquare = new Square();
+  console.log(newSquare.isSquare(2, 2, 2, 2));
+  
+  
+  
