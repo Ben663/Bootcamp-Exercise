f614254c7fc6c53642e3dcf6f50deef1326cb15b
Ben663 <87435130+Ben663@users.noreply.github.com>
2022-10-25 15:08:14 +0300

    edit

--- js/promises/promises.js
+++ js/promises/promises.js
@@ -1,14 +1,18 @@
-const Myage = new Promise ((resolve, reject) => {
-    const age = Math.random();
-    if (age < 10) {
-        resolve();
-    }else {
-        reject();
-    }
-});
-Myage.then(() => {
-    console.log(`the age of you is less`);
-});
-Myage.catch(() => {
-    console.log(`the age of you is greater`); 
-});
+function Check(num) {
+    return new Promise((resolve, reject) => {
+        if (num > 10) {
+            resolve();
+        } else {
+            reject();
+        }
+    })
+    .then(() => {
+        console.log(`the number of is greater `);
+    })
+    .catch(() => {
+        console.log(`the number of is less`);
+    });
+    
+}
+Check(6);
+Check(16);
