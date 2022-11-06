56eb2b0ff0379a81ad09d531d9ea8db456e984cf
Ben663 <87435130+Ben663@users.noreply.github.com>
2022-11-06 23:23:09 +0200

    221106

---
+++ react/Ex5-1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Button from './Button';
+
+
+
+function App() {
+  return (
+    <div>
+      <Button font='bold' />  
+    </div>
+  );
+}
+
+export default App;
--- react/Ex5-2/src/App.js
+++ react/Ex5-2/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-// import Button from './Button';
 import Card from './Card';
 import imges from './img/one.jpg'
 import imges1 from './img/two.jpg'
@@ -10,7 +9,6 @@ import imges2 from './img/tree.jpg'
 function App() {
   return (
     <div className='first'>
-      {/* <Button font='bold' />   */}
       <Card title='hello world' description='good day' photo={imges}/>
       <Card title='hello world' description='fun day' photo={imges1}/>
       <Card title='hello world' description='happy day' photo={imges2}/>
