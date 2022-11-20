fe18e66deed86fb650a69d5c060f5b3fe5ab550a
Ben663 <87435130+Ben663@users.noreply.github.com>
2022-11-21 00:11:57 +0200

    timer use class coponent

--- react/forevreting/src/Components/App.js
+++ react/forevreting/src/Components/App.js
@@ -1,30 +1,33 @@
-/** @format */
 
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import React from 'react';
 // import Head from './Head';
-import Input from './Input';
+// import Input from './Input';
 // import Todo from './Todo';
+import Clock from './Clock';
 import './App.css';
 
-// const Box = styled.div`
-// 	background-color: #282c34;
-// 	min-height: 100vh;
-// 	display: flex;
-// 	align-items: center;
-// 	flex-direction: column;
-// 	justify-content: center;
-// 	font-size: calc(10px + 2vmin);
-// 	color: white;
-// `;
+const Box = styled.div`
+	background-color: #282c34;
+	min-height: 100vh;
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	justify-content: center;
+	font-size: calc(10px + 2vmin);
+	color: white;
+`;
 
 function App() {
 	return (
 		<div>
+			<Box>
+			<Clock />
+			</Box>
 			{/* <Box>
 				<Todo />
 			</Box> */}
-			<Input ></Input>
+			{/* <Input ></Input> */}
 			{/* <Head>
 				<h3>hello</h3>
 			</Head> */}
